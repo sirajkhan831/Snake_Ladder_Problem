523597012457365bbaf013d08527d43651547016
Siraj <sirajkhanc2305@gmail.com>
2021-08-18 17:52:50 +0530

    PartTimeEmployee

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -3,11 +3,22 @@ package com.bridgelabz;
 public class SnakeLadder {
 
         public static void main(String[] args) {
-            double attendance = Math.floor(Math.random() * 2);
-            if (attendance == 1)
-            {
+            double attendance = Math.floor(Math.random() * 3);
+            int WagePerHour = 20;
+            int fullDayHour = 8;
+            int DailyWage;
+            if (attendance == 1) {
                 System.out.println("Employee is Present");
+                DailyWage = WagePerHour * fullDayHour;
+                System.out.println(DailyWage);
+            }
+            else if (attendance == 2)
+            {
+                int partTimeHour = 8;
+                System.out.println("Employee is part-time employee");
+                DailyWage = WagePerHour * partTimeHour;
+                System.out.println("Employee's Daily Wage is : " +DailyWage);
             }
             else System.out.println("Employee is Absent");
         }
-    }
+}
