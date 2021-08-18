869d115f909fa7151f32e83168008abeeef14e28
Siraj <sirajkhanc2305@gmail.com>
2021-08-18 17:50:32 +0530

    DailyWage

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -2,12 +2,14 @@ package com.bridgelabz;
 
 public class SnakeLadder {
 
-        public static void main(String[] args) {
-            double attendance = Math.floor(Math.random() * 2);
-            if (attendance == 1)
-            {
-                System.out.println("Employee is Present");
+            public static void main(String[] args) {
+                double attendance = Math.floor(Math.random() * 2);
+                int WagePerHour = 20;
+                int fullDayHour = 8;
+                if (attendance == 1) {
+                    System.out.println("Employee is Present");
+                    int DailyWage = WagePerHour * fullDayHour;
+                    System.out.println(DailyWage);
+                } else System.out.println("Employee is Absent");
             }
-            else System.out.println("Employee is Absent");
         }
-    }
