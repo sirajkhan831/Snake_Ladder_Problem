d5460d234b0adacabd934cb7b70f676f8e541645
Siraj <sirajkhanc2305@gmail.com>
2021-08-18 17:51:20 +0530

    IssueFixed

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -2,14 +2,12 @@ package com.bridgelabz;
 
 public class SnakeLadder {
 
-            public static void main(String[] args) {
-                double attendance = Math.floor(Math.random() * 2);
-                int WagePerHour = 20;
-                int fullDayHour = 8;
-                if (attendance == 1) {
-                    System.out.println("Employee is Present");
-                    int DailyWage = WagePerHour * fullDayHour;
-                    System.out.println(DailyWage);
-                } else System.out.println("Employee is Absent");
+        public static void main(String[] args) {
+            double attendance = Math.floor(Math.random() * 2);
+            if (attendance == 1)
+            {
+                System.out.println("Employee is Present");
             }
+            else System.out.println("Employee is Absent");
         }
+    }
