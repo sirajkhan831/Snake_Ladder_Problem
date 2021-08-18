c126c7bebe0de27f0e4cb3b55783720ce22b96c4
Siraj <sirajkhanc2305@gmail.com>
2021-08-18 17:42:47 +0530

    Added Employee Attendance

---
+++ src/com/bridgelabz/SnakeLadder.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class SnakeLadder {
+
+        public static void main(String[] args) {
+            double attendance = Math.floor(Math.random() * 2);
+            if (attendance == 1)
+            {
+                System.out.println("Employee is Present");
+            }
+            else System.out.println("Employee is Absent");
+        }
+    }
