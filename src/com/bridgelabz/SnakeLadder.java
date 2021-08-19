c397ae1308a5d1ad1b25684ff969938a9e0a8688
Siraj <sirajkhanc2305@gmail.com>
2021-08-19 16:46:02 +0530

    AddedSnakesLadders

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -4,9 +4,14 @@ public class SnakeLadder {
 
     public static void main(String[] args) {
         System.out.println("You got : "+roll());
+        luck();
     }
 
     public static int roll() {
         return (int) Math.floor(Math.random() * 6) + 1;
+    }
+
+    public static int luck() {
+        return (int) Math.floor(Math.random() * 3);
     }
 }
