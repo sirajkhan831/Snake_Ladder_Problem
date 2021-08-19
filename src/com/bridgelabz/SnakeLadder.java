c5aeb9110c82d7113e8b39e3c7a2a0f57b67b6f7
Siraj <sirajkhanc2305@gmail.com>
2021-08-19 16:51:34 +0530

    PlayerWin100

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -3,8 +3,26 @@ package com.bridgelabz;
 public class SnakeLadder {
 
     public static void main(String[] args) {
-        System.out.println("You got : "+roll());
-        luck();
+        int store = 0;
+        while (store <= 100) {
+            int roll = roll();
+            System.out.println("PLayer1 got : " + roll);
+            store += roll;
+            System.out.println("Player1 is now at : " + store);
+            if (store < 89 && store > 11) {
+                switch (luck()) {
+                    case 1 -> {
+                        store += roll;
+                        System.out.println("Player1 got a ladder is now at : " + store);
+                    }
+                    case 2 -> {
+                        store -= roll;
+                        System.out.println("Player1 got a snake is now at : " + store);
+                    }
+                }
+            }
+        }
+        System.out.println("Player 1 has won");
     }
 
     public static int roll() {
